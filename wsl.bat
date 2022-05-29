aba1811429176951e0ac57ca8245a173deee4d58
BrianWong05 <55014166+BrianWong05@users.noreply.github.com>
2022-05-29 20:05:55 +0800

    Create wsl.bat

---
+++ wsl.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Unrestricted -File ""C:\User\wsl.ps1""' -Verb RunAs}";
