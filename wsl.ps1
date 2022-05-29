05f1185df5b3e894b0bf363a156615976bb5cc37
BrianWong05 <55014166+BrianWong05@users.noreply.github.com>
2022-05-29 20:02:27 +0800

    Update wsl.ps1

--- wsl.ps1
+++ wsl.ps1
@@ -27,7 +27,7 @@ iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' "
 iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
 iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
 
-for ($i = 0; $i - lt $ports.length; $i++ ){
+for ($i = 0; $i -lt $ports.length; $i++ ){
   $port = $ports[$i];
   iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
   iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
